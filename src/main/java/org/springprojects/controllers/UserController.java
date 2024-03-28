e4a2a8e623954db8289710c026c310b675c76200
Petru-Jean <carciumarupetru@gmail.com>
2024-03-28 15:43:50 +0200

    Implemented UserController and user retrieval Modified repository to user UserDTO instead of Domain Object

--- src/main/java/org/springprojects/controllers/UserController.java
+++ src/main/java/org/springprojects/controllers/UserController.java
@@ -1,13 +1,34 @@
 package org.springprojects.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springprojects.dto.userDTO.ViewUserDTO;
+import org.springprojects.repositories.UserRepository;
+import org.springprojects.services.UserService;
 
 @RestController
 @Validated
 @RequestMapping("/api/v1/users")
 public class UserController
 {
+    private final UserService userService;
+
+    @Autowired
+    public  UserController(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    @GetMapping("{username}")
+    public ResponseEntity<EntityModel<ViewUserDTO>> getUser(@PathVariable String username)
+    {
+        return userService.loadByUsername(username);
+    }
 
 }
--- src/main/java/org/springprojects/services/UserService.java
+++ src/main/java/org/springprojects/services/UserService.java
@@ -1,14 +1,21 @@
 package org.springprojects.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springprojects.controllers.UserController;
+import org.springprojects.dto.userDTO.UserMapper;
+import org.springprojects.dto.userDTO.ViewUserDTO;
 import org.springprojects.entities.User;
 import org.springprojects.exceptions.NotFoundException;
 import org.springprojects.repositories.UserRepository;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -20,10 +27,24 @@ public class UserService implements UserDetailsService {
     {
         var user = userRepository.findByUsername(username);
 
-        if(user.isEmpty())
+        if(user == null)
             throw new UsernameNotFoundException("Username '" + username + "' not found.");
 
-        return user.get();
+        return user;
+    }
+
+    public ResponseEntity<EntityModel<ViewUserDTO>> loadByUsername(String username)
+    {
+        User user = userRepository.findByUsername(username);
+
+        if(user == null)
+        {
+            throw new UsernameNotFoundException("User '" + username + "' not found.");
+        }
+
+        ViewUserDTO userDTO = UserMapper.INSTANCE.map(user);
+
+        return new ResponseEntity<>(EntityModel.of(userDTO, linkTo(UserController.class).slash("").slash(userDTO.getUsername()).withSelfRel()), HttpStatus.OK);
     }
 
 /*    public User loadByUserDetails(UserDetails userDetails)
